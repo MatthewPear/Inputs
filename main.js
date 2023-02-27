00f84bb6777af6bab422a8dcd6f1fb88d0445cb4
MatthewPear <matthewpear@hotmail.com>
2023-02-27 23:17:36 +0100

    mk3

--- main.js
+++ main.js
@@ -8,30 +8,34 @@ function passValue (value) {
     container1.textContent = value;
 }
 
-const input2 = document.getElementsByName('macanie');
+const input2 = [...document.getElementsByName('macanie')];
 const btn2 = document.getElementById('button2');
 const container2 = document.getElementById('container2');
-console.log(input2);
-btn2.addEventListener('click', () => passCheckBox(input2.chilNodes.checked));
+console.log('input2', input2);
+const result2 = [];
+console.log('result2', result2)
+btn2.addEventListener('click', () => passCheckBox(input2));
 
 function passCheckBox (list) {
-    //console.log(list);
-    if (list.childNodes.checked === true) {
-        container2.textContent = list.childNodes.value;
-    // } else {
-    }
+    list.forEach((el) => {
+        if (el.checked === true) {
+            result2.push(el.value);
+            container2.textContent = result2;
+    //  } else {
+        }
+    })
 }
 
-const input3 = document.getElementsByName('alko');
+const input3 = [...document.getElementsByName('alko')];
 const btn3 = document.getElementById('button3');
 const container3 = document.getElementById('container3');
-console.log(input3);
+console.log('input3', input3);
 btn3.addEventListener('click', () => passRadioChoice(input3));
 
 function passRadioChoice (choice) {
-    //console.log(choice);
-    if (choice.childNodes.checked === true) {
-        container3.textContent = choice.childNodes.value;
-    // } else {
-    }
+    choice.forEach((el) => {
+        if (el.checked === true) {
+            container3.textContent = el.value;
+        }
+    })
 }
